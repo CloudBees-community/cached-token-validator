0d4a26bb5a78f791f1e33d10ebf33af3d9f7223e
Kohsuke Kawaguchi <kk@kohsuke.org>
2013-08-14 09:51:29 -0700

    build test jar and test source jar

--- README.md
+++ README.md
@@ -23,3 +23,21 @@ Usage
 
     OauthToken t = v.validateToken(tokenThatCameFromHttpRequest)
     if (t==null)    throw new SecurityException("invalid token: "+tokenThatCameFromHttpRequest);
+
+Mock Test
+---------
+When you are testing your code that uses `TokenValidator`, it is often convenient to be able to
+run tests without talking to the actual OAuth server. For this purpose, this library provides
+`MockTokenValidator` in the tests jar.
+
+To use this, add the following in your POM as a dependency:
+
+    <dependency>
+      <groupId>com.cloudbees</groupId>
+      <artifactId>token-validator</artifactId>
+      <classifier>tests</classifier>
+      <version>1.0</version>
+    </dependency>
+
+This class constructs `OauthToken` object just from the textual representation of the token
+it gets, such as "account=kohsuke,scope=...". See its javadoc for the details.
---
+++ src/test/java/com/cloudbees/api/MockTokenValidator.java
@@ -0,0 +1,65 @@
+package com.cloudbees.api;
+
+import com.cloudbees.api.oauth.OauthClientException;
+import com.cloudbees.api.oauth.OauthToken;
+import org.codehaus.jackson.annotate.JsonProperty;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * {@link TokenValidator} for testing purpose.
+ *
+ * This implementation is useful for unit testing your code that uses {@link TokenValidator}
+ * so that you can run offline without talking to the actual grand central.
+ *
+ * This implementation expects the access token in the format
+ * "key=value,key=value,key=value,..." where each key is the JSON property name
+ * of the {@link OauthToken}, such as "account" and "access_token".
+ *
+ * If you pass in the token that starts with "invalid", the token validator pretends that
+ * the token have failed validation.
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public class MockTokenValidator extends TokenValidator {
+    @Override
+    public OauthToken validateToken(String token) throws OauthClientException {
+
+        // cue to return null?
+        if (token.startsWith("invalid"))
+            return null;
+
+        OauthToken oa = new OauthToken();
+        for (String t : token.split(",")) {
+            String[] lr = t.split("=");
+            if (lr.length!=2)
+                throw new IllegalArgumentException("Malformed mock token: "+t);
+
+            Field f = FIELDS.get(lr[0]);
+            if (f==null)
+                throw new IllegalArgumentException("Invalid property: "+lr[0]+" expecting one of "+FIELDS.keySet());
+
+            try {
+                f.set(oa,lr[1]);
+            } catch (IllegalAccessException e) {
+                throw new AssertionError(e);
+            }
+        }
+
+        return oa;
+    }
+
+    private static final Map<String,Field> FIELDS = new HashMap<String, Field>();
+
+    static {
+        for (Field f : OauthToken.class.getDeclaredFields()) {
+            f.setAccessible(true);
+            JsonProperty p = f.getAnnotation(JsonProperty.class);
+            String name = p!=null ? p.value() : f.getName();
+            FIELDS.put(name,f);
+        }
+
+    }
+}
